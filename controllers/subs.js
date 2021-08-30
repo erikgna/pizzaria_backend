16f69ec83ecb4d2af2f8eb9244e5e4fd2f7ce973
Erik Na <erikgnaa@gmail.com>
2021-08-30 11:09:22 -0300

    Update subs.js

--- controllers/subs.js
+++ controllers/subs.js
@@ -1,4 +1,5 @@
 const Subs = require('../models/subs.js')
+const Sabores = require('../models/sabores.js')
 
 const getSubs = async (req, res) => {
     try {
@@ -11,11 +12,25 @@ const getSubs = async (req, res) => {
 }
 
 const createSubs = async (req, res) => {
-    const {name, value} = req.body
-    
+    const {id, name, value} = req.body
     try {
-        const createdSub = await Subs.create({name: name, value: value})
-        res.status(200).json(createdSub)
+        const nameExist = await Subs.findOne({name: name})
+        if(id === '' && !nameExist) await Subs.create({name: name, value: value})
+        else {
+            await Subs.findByIdAndUpdate(id, {name: name, value: value})
+            const sabores = await Sabores.find()
+            let temp = sabores
+            temp.forEach((item) => {
+                if(item.categoria === name){
+                    item.categoryPrice = value
+                }
+            })
+            temp.forEach( async (item) => {
+                if(item.categoria === name)
+                await Sabores.findByIdAndUpdate(item._id, item)
+            })
+        }
+        res.status(200).json('createdSub')
     } catch (error) {
         res.status(400).json(error)
     }
